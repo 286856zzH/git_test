535ea58044a3b80a71a8e91f591e653ee5fd974b
zhang <zhang@qq.com>
2022-04-23 14:30:17 +0800

    first commit

---
+++ src/main/java/com/zhang/hit/GitTest.java
@@ -0,0 +1,11 @@
+package com.zhang.hit;
+
+/**
+ * @author 张老板
+ * @create 2022-04-23 14:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
