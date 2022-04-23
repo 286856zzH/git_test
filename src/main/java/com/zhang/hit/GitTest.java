96d7aa2265104e845ff70144dad839bfded5aa9b
286856zzH <81243625+286856zzH@users.noreply.github.com>
2022-04-23 15:48:11 +0800

    aaaaa

--- src/main/java/com/zhang/hit/GitTest.java
+++ src/main/java/com/zhang/hit/GitTest.java
@@ -7,5 +7,6 @@ package com.zhang.hit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.pringln("hello github);
     }
 }
